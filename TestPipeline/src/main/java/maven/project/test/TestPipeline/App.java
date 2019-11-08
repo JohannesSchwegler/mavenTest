ca32c5ba35c49e4d8256f0d2165c498427bb6579
Johannes <johannes.schwegler@outlook.de>
2019-11-08 11:46:36 +0100

    -- Inital Commit

---
+++ TestPipeline/src/main/java/maven/project/test/TestPipeline/App.java
@@ -0,0 +1,22 @@
+package maven.project.test.TestPipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+    
+    
+    
+    public int multiply(int first, int second) {
+    	
+    	return first*second;
+    }
+}
---
+++ TestPipeline/src/test/java/maven/project/test/TestPipeline/AppTest.java
@@ -0,0 +1,29 @@
+package maven.project.test.TestPipeline;
+import org.junit.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+	 @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    
+    /**
+     * Test2  :-)
+     */
+    @Test
+    public void secondTest()
+    {
+        assertTrue( true );
+    }
+
+}
